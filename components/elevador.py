a3517b973928bd8ed8758211fad37c43ef63c1dd
Patrick Strassburger <patriick.dev@hotmail.com>
2023-01-19 22:39:31 -0300

    projeto pronto

--- components/elevador.py
+++ components/elevador.py
@@ -3,9 +3,20 @@ class Elevador:
         self.__andar = 1 
 
     def locomover(self, andar): 
-        ...
+        if (andar < 1 or andar > 15): return self.__mensagem_de_erro()
+        else: 
+            return self.__alterar_andar_e_retornar_informacao(andar)
 
-    def mensagem_de_erro(self, andar): 
+    def __alterar_andar_e_retornar_informacao(self, andar):
+        self.__andar = andar
+        if (andar == 1): return self.__mensagem_de_alteracao_para_terreo()
+        return self.__transitar_andares()
+
+    def __mensagem_de_erro(self,): 
         return (f'Andar incorreto! Elevador no {self.__andar}° andar') 
 
-    
+    def __transitar_andares(self):
+        return (f'Elevador indo para o {self.__andar}° andar')
+
+    def __mensagem_de_alteracao_para_terreo(self):
+        return 'Elevador indo para o térreo'
